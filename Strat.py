b08d5db6ffbb0961e8be23fce6b98d4f5d8d120d
Malank Goel <goelmalank10@gmail.com>
2025-09-30 12:40:13 -0500

    Changes

--- Strat.py
+++ Strat.py
@@ -8,7 +8,7 @@ import pandas as pd
 import streamlit as st
 import plotly.graph_objects as go
 import plotly.express as px
-import alpaca-py
+
 
 from configuration import FEATURES, LIMITATIONS, GPT_SYSTEM_PROMPT, MODEL_NAME
 
